deaf1ebeb6cf442cd926f8b34cd04783daa0e6b8
r-n06 <19nr96.xx.o6@gmail.com>
2020-11-11 11:43:02 +0900

    新規投稿機能作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :image
+  end
 end
