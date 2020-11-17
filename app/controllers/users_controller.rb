1d6c8f1843f9f0ce2fd1e573ca485a3ecedd3a68
r-n06 <19nr96.xx.o6@gmail.com>
2020-11-17 18:33:43 +0900

    マイページ表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @posts = current_user.posts
+    user = User.find(params[:id])
+    @posts = user.posts.order(created_at: :desc)
     @nickname = current_user.nickname
   end
 end
