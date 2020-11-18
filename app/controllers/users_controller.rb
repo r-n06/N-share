5d5c3b1f45c2e8decccf17bda7c9cc1571be4785
r-n06 <19nr96.xx.o6@gmail.com>
2020-11-18 15:54:11 +0900

    マイページ編集の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
     @posts = user.posts.order(created_at: :desc)
     @nickname = current_user.nickname
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+  end
 end
